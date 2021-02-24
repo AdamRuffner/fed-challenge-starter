7a2d8fbf3e09e13b6d20e7297a1d9536f55c450c
Adam <ruffneradam@aol.com>
2021-02-24 18:08:30 -0500

    updated the rest of the image urls

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,12 +8,12 @@ function Cards() {
     <div className="card-container">
       <Card
         src={
-          "https://uc058f8f3ce2429fcf201042caf8.previews.dropboxusercontent.com/p/thumb/ABGJXvWTvRsIPXLcfcOKIS1X-mBjKJ_-uVL7ufZpueODc0RYz3npzW4AaM79FGf2Z73IR5lkPtUfYCPK13wCy_xzbXxgA4nQGWt0miEvesVIo3rp_6XUwb1gYvdqGBzj24RIEN5NjLzRijvC8Y-w0inDDSKAj5I9oPqwhmw7lNPUu05ZrHGF4HkQ-_5NL0oaxbU-oOpBO0LTg0rix5mPEvTY5LsF9odX-kxh2V-pUdtTPqahY25HQL6FGlVy8IQlUnakRA4I28vN9XJd3PBsZHOVmD0KWwv1nlP1OhwEtoYZNAuyxjcyVdg9HIIpP7rNqQALbVeQuz-XirERH21F5e2NraJ051FnwI4YYoYkG0y1Gqr9uox4yhlTpmJlXxbJWx6TO4Na7LHT6HI-FCa2hn9K/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc9dec2270196561c7744807e49e.previews.dropboxusercontent.com/p/thumb/ABGhLq9P0EO88QAFEhU5XHtaRZj2QKCOMS3BsZYa_XDLXaRfNxAMB3BGmQ45I6KUMpYLBmUAvczrocIzIa-pjQL4F4s61anvzf12tE62SpVgvSYXar5i7KP2Vf_lVQVdiqiLJJ14SUiG9RBiuYNpwEdqcJ7OGlqxUT3KJF5Zd9xbG3YikCdD94Yp9gnTuo4tMbbwu54SV_e_DFmQsvHaLlixXUnMJh6V_CjbY7-RTeO1tpnttUJ14kIax1d0E9-LlAHkb_wylEyySL9ku1qjCppIT5-kIS290Lu9YV8JYr_YTbvTbXuD-Bz7pEdJaoT0I1owUxCsBKIbZ4SwmeRBtKxwVL-RBG9uU-iQra1bcf_pIA/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"lake inniscarra"}
         description={"Lake Inniscarra, Ireland - Pyramid"}
         profileSrc={
-          "https://uceb39464f3416e8522411e2018b.previews.dropboxusercontent.com/p/thumb/ABFb_gHfa_a7pPVuHojMTMJ2RMJDwZAfn61QPvNxM4rFXnl7R9c-fi1Ya36__2rrZodvIrlFwFBOhESE6J1Fw-KQR1NaVy5slW2leghoB1vKNuqi0kC73Cag8zKuLgsfcwP0O-Kln4oYCp5H-j0SsQDFSqv87Ub7XhU8C-D2ruPGEpsS_jUxylGMyU7ZOytP8o2RYB0WanBdQPJY7DmjIMqSycuBppef6m4RX6bFv2v6uMMW-xvAdr41REJi-AojPzN6vIOUrxa-WRCJS2YI4crKQ0m4ZXoUyR3-eBRjqCS9vqGxaunM8gcZmncVKU9gPNnevxFrxCRR3B_unyoexyzazu22H36P4BFVWLRCk7lEGA/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc1c92712322452cc26a36cfd4d0.previews.dropboxusercontent.com/p/thumb/ABE8v79DnXWrkKPrNoIDohU5Gg6o3_5aiI8JuBKeul1wzxr03AZL-dwoBkWttnfmoDSeYhiG3y4p1eTkYpDJef55Kkb_xhdLJAfDIlHESS6dU5N8wiF1HOABuhlUMCCruSG6FMRAz9NJf0hmDt6Ug1Xq_gA9--glV6j73HUmEJrfvyV1Jq21eBnpkCjDsSVIBiJCDA4h6MVH_xKD25-vRrBor4_44tCUjsheInQYXc3tSR7FHzTBThy6w4hLONgJrNDlVcKOaMHNKQ44egskTV5cXMQoMid3S_pgqCpdr2VaVDnYwVcBLXlrSgxUEiCVEOXaBCalV2Tyw9FLDaTzD0MX8qMiMuagn-_F931QnyuiIg/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         time={"30:53"}
@@ -23,12 +23,12 @@ function Cards() {
 
       <Card
         src={
-          "https://ucc2f7554ba19f6feaa37ed63e70.previews.dropboxusercontent.com/p/thumb/ABEnWM0K01rMOUwdngPm00hw2V1oW6xFY97OeeM2blUb0bvXFGpC1TpwdwapvYs2uR8AcZm-mR7eWKI-y7aIr60JzLtMDcQDeqBf4PMpTINIiWmRLkmfQd4QtPM6_Z-an3AsJpGCKn7Sui2ysRllEKlpMAip327MsbqxZhJlvrL1anD2-mYRjx4euBqfdK0CyLb68m0DB-o8iUwcsIhxJkGsd49pqmW3P9i3ccV5bIYR5WKJgvdqKtBHq9X_O5Ua75Crlg4hqtGCtyKr_fZqcgevqfH7gW87bXMNHveG2AMS1J-BTWKe8pSKsDd-3MheTi08XKh1W4KxcGIVbbj2GWdEyvV-HIG7ImJnDjpMIG5_4g/p.jpeg?fv_content=true&size_mode=5"
+          "https://ucdcd18a7dc49b2b1190ffe0a5c8.previews.dropboxusercontent.com/p/thumb/ABH8Ru20tfvPXXNhixK88TP4Ay7ATlK9SdF1T_6ILmHbzvMGx0UCTlSKtBIGSArna0sKnYkup06kHpmF-1ohRNjsIHAha2gpon06iror1h-9qeyvtkuZD51TIK7OPiJJ3J9oNf_mtY-IZyx-kZRLTZOa_ksWPMVIKGdnPOD53NDT9Yf6Pd6UFwPXD16VmFNv4BJfWcq9p_z0anIdaPU4UMbynTpenVNXrQHvlvwmEDUM3pGukw9JJzfHAcbHg4yhhizk7z1JnO5ceJfSd6IYpAqvKYX4fpSJaZ3G_butLOr-a2fYp9nOZYaL2xh5lUWJyWvJFJexqqb1_rM7-vUiNZyxXvpbKZjqmVc6slVagN_oHg/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"performance series"}
         description={"Performance Series"}
         profileSrc={
-          "https://ucda3446287a840b844a91f33ba0.previews.dropboxusercontent.com/p/thumb/ABESkXeHX7IHsje_cJT4oHjr48vIM7Ndv2HM-GgmNpoL1FQpcJZmvuyAWuxOZbE-DX_dUK11pwt3UHWcWO_a2XScZjIoIgBH5lnq4PYf4QQ6bwObjQLqwvKSCv3nrI5-7xRmPkzm1QTygivPv5tW7-MiVX76qoXCsIFBgKI6jqsD3pKs9J6R6GFOL2IXKAODbpEUK1aped17ZKduyA_FpXsQJx0PRUesdkk9T6oQcMMFi8AWejPXNE62fCq3TwVY9I7YV8wMjXw84ozBJt9Ncy5jJQA9kkUJO_fUDFU6DY-KRg8PxQRdO68Af1z4y7uEZSmU1LMu_vmGsAWjOkojewJCXnxY7V8f8CPispx9_WWDfg/p.jpeg?fv_content=true&size_mode=5"
+          "https://ucefb9fc0f17dde4eedd54ea96ae.previews.dropboxusercontent.com/p/thumb/ABG7N4FgChONbdtzE5bLE_cCwZebdXD9wgYG_sHaX-3lpwxlfBGZE9ISQ5ZqRD3lh-BeGW-9bVgOBfjI_QvGiD3aovDeNAG_ODKgMTJjfVAXXXvoepK1cc41RAjfFLeA59VlU1JsA9fD3-O6K0nUL-UpbA1vRiYClJzw64y2mhAKSBa_FbmLGlp5RqJolBaGAux9kg3H9MEMAoFkhPZH_WqY9ZAQqWr6YOnjmJSfpaB8U6pHYito0ndjE4TlzHaU2f-aIyKDZTb0qRbKdZACDHTPfZuB657qzslxLbcaaSF6ZIurfsTvPzMXGjqRPEjaiJJZrEdgypwsY-3rTnLfjrWLC7iGLsKgXiJ0PWzlpSBhMA/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         icons={false}
@@ -36,12 +36,12 @@ function Cards() {
 
       <Card
         src={
-          "https://uc11603dc908ab9b306eb3d71335.previews.dropboxusercontent.com/p/thumb/ABHzD_frHgRGyTBEni_s29FcCpEoxe3DOMJNAtTIDu42Ky6A0Ye8ZAAgO7idFqID_CRCHNWRZ3F91aEUTPtIQi1qsDJt1qwEWfCZLkcSGjqxTWTzpcrO5KJ2cfwX0lfMNlu423Ds5m-Q0PoXAhwekW9gXXmId0jAadhLFfXDqu_tePqa77xkTT4i83xcbsWGPeCRzxgOlk2onnUkGy_7Mc2cd2OiFXYiY76qSPN2sg42YfTsUJkY4x9_N9VRjWvwzY-FL3_1Ybu0GpOQynZoAQnc_W8sEOngWE9Prh0_vWGm0XumGiZ5TA6j0-XEW-B6U0nQ-mLq0SdsNWLeg8O7Qt2JZUTzwOLFppH68G5j7IOjbA/p.jpeg?fv_content=true&size_mode=5"
+          "https://ucb7c5b54ba7b3f17eae1f8b370a.previews.dropboxusercontent.com/p/thumb/ABGM7hD56sVFGomEJMhajBrgcSmjiFfeXseZFYnELzbWY8GwkydQxHtbBqw40foYDXO0n55xwxD87h2HGMOg_4f8aFfgPASGsFYfyoXiEyQSbZga83UyM5sdkm9aNAERpgfGs0A_TOEzCEYrYwnKaRwA7VVmABS3GoLcDiFlUHFHTPV4DdB6q1roK7UheUqX883B1Dk_xvIQrMeLCQyEJCSi-JVeXC7azbOknOt5D5xszY53mXn48ZxZapSI1la51zTt79bpay8KL9b-I0VUuWpS4pgUCtttZcwJndp1qm6SZhm8B0Ltw62ZH3ARPg86U7kL2queHxZPoheOfuCGGynhxS0W_uiGfoExVLvCa5m_0w/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"slow pulls and fast intervals"}
         description={"Slow Pulls and Fast Intervals"}
         profileSrc={
-          "https://uc5826f7051e31e0a02f1925f930.previews.dropboxusercontent.com/p/thumb/ABFA8MrO-qYEfvTpFHtmLC7j8ehll2pfhPRQJVLk1j--fQxhBRFqp2pdaXXoiRPDIPnMWCuL352qJe7Hti_f7XmGWaQi3vQ-6rX5jSnbHyyN5FQh2Og2tdxGBM60MVIpf73LIIftb8PlF6uZrFQOB9ZYFxMIks2dKsdaqhjINeljCLwT-UD29OG_XsP7s5AkyM589zfb-G1cEkbLST1IAZSvtJLGdvaw7J9mGTtoZChyB1LRsKnzgUiatFMzuqIiO4hsy021YvlNQHF8RoN9qwwyHXIH7MgF62uEmchbiSRvaEem0eKJ9CyMCWyVD916-2uWZi-wHI5D2GaKN0fKCYxDAysiY9NX1xmRyYuoKIeDfA/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc95fa7808982f4148d2b267fb99.previews.dropboxusercontent.com/p/thumb/ABEpfCTtXE56RdkG430rOgdrRrEO6twKELe4lNWJ8k-vDY1pPTLLIEt_Q1KorGxzidF7WgBI-ndw1mk4T3I78qPDhqigkWGEADO7EAnjQn5H31qWWoZSx4MN3diU_H734sfs8BMCQ_CHB_QPh3BgztIMa27JKsUGC5C_4urRht277qywsEkxrQ1Mwuqsnyd4mgs2qyZ4m0YmKrZJlFdbz1CZqejOwzIPhroLasKgosvsfEgGPqFUvJp_swkshxJUFZOf7KUoRafuNe8sT7j-UZZxd_8daMyoceijtXOcV_mGORBEg3FFxh_-0GILAVotDb3bdMLRN7trt1J84HLIhsrLGuzvXM8mX0-FTiHILGuFTg/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         time={"44:13"}
@@ -56,7 +56,7 @@ function Cards() {
         alt={"header image"}
         description={"20 Minutes to Toned"}
         profileSrc={
-          "https://ucec91807b54546229ebe409b5a0.previews.dropboxusercontent.com/p/thumb/ABHNRtVtI5FHHlKqbyzy4sqwAhQ23lKXPIhutDp2SaivE37behWMovfARqUkMuOa4gaauF_7ZCDClKMUfoFShNIf3WqWsXSQ8akCWYiY9QaKFPpFhvvSAPVsq5Xu6wDDMn-KvQket91vudL8ZKvW5uqWElGVVIdKspu-vWs3wPxrTWbS8J0M1mMvjN_uUcaFaQV1GIP5Rw-8NFRgmrKEgTkyPZuyi_CykvbTJao6h6Zl9N1xamUra59z_0ECVEXucgHprKhPD5Ny4Sc5a0ik8ORSGxmr57jm8y5wIDm6kRE53b5I87kGO2c_SN5MZljE3PiYTskY339DKxzPyRvc718f6Hu0yoPqcKCB7TTcY7_H1w/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc9382d99a4e572313540873e561.previews.dropboxusercontent.com/p/thumb/ABEwTBNjpjogeuDe2IZYCKGKaxGwaM7aRW9kwuT5w8yOwfuu6wMAQzE1yBuvWfexTEAG7zWZE2MUlCRlmBx4zj6w6J8-9hh1Bg1a6ZBc-yBElMWQOcvXAYWXIE4JCg9VvQpcKNZbGKt6bdXsOl4U23RBUgDQBg93FQTgNYz7LOowVkQoTVCGhypiGM9vi51CcHgYXkPGj2XTKNfEBFQAUikQrK_sk2EZtg16wB5zDO-d2SIKSntf4UZ3bpkRf_8eOvagak4Z1x_7_BtkIQJIi5tCb_DdZRZYhlyyvaBfQjObA6a2qS8knPrgIA4aAxoHGl_-L3cJw2Px8wtaMRuS465aE8VbzD-OBQZ3Mwtgm0WsZg/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         icons={false}
@@ -64,12 +64,12 @@ function Cards() {
 
       <Card
         src={
-          "https://uc6ec16241f650d6122c365b7176.previews.dropboxusercontent.com/p/thumb/ABE7OnY0DVTwvUx57OlLEPMeMUmD_hL0AXXPJnpCuKVi5kNfdCgs95-_YQDbzVZKrYQAJrInabcYIRbTN84nRerJ5AOxPDBay1Gu1i715BOgsZxBk1pvC8eBQlLqP2F7Vh6WGhwjNlEzS53AqWYyMcB1fhvSVNSel6otBSgH5tVzqNrLqaedo9XyURneWPKUGxdKKHHNAoTTryv5pZUZNeoHjRm36v-mC6LeTTYySk3UwRto7fxTjR0ghf95xzYDEUoy84W1i-3kdtXvvfYM1cgj0vN5faEaDcrQWJK-Q_hBrOXIdQ5LK3QDIUWkrFQ2sASaax3BpjZS_vRwfXoNYSbbO7IO_4SarYDnh30T3VwRFw/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc8c50e1e7cabf32157d620a6c9d.previews.dropboxusercontent.com/p/thumb/ABEWwgSt5laXlShqoe6xbHoRieY-ClSVTWzSk8aHNaUF9PdCYZKBMb0aVJ1pBZv-zuM-zpjw6aSqZwgwXaeHvxf2201sJ0uIYVweOwnDOogdmFK0jBeojWxSBMLGsnfleUZRRjFvUIriDOuSxkbZ2-DGYnIf0rzYgZ5ohrRMZaabHwdUWPgHYR3qZ0P-6YmwY-6FEd1w_C_JiZV5Xkl9qpLPnUeBfUDbca7BW9u_qUwEUSKdcJDDunxM0Qt7Xl3nJXORGjH866HOlTn2LhqCpHTh4uFQPnqrW_zjP3mq3SwTaGz7IHW-M4cDl1_kwJkqM8F6NCFDLqE_Pobj9aVcKLWN5zqWnszeot60WrbSPvfROQ/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"Charleston Race"}
         description={"Charleston Race, Boston, Massachusetts"}
         profileSrc={
-          "https://uc0b40a30b69450ae6dc007bb5da.previews.dropboxusercontent.com/p/thumb/ABHXFJ1MrnepkosKJuZroypp8yZ6zDY4vSDXj45DutAWe9LlyZ7Uk6fnDMS1MZedohIgJT8RxfN5Vcfr6I8_8J8YQZOAOaKL9aZu5HXVojH0VOjz1miLTx3C0lULyUvxfw87IKpsFnbwCzzQ7tV1ju4Bhn1dGRJ8OH7XiHaX0zc2FlxFYQJRHe3wJDLFSKb9odtuK-2_GOjkhhYb4PNaN2pPG_wAJ6E45Alu0OKQt7Bwt8-f2eZR8889L9iMLMwS0ik9pI3dOVUdeB3Vwez-lLHrLJYnUOP6RKKQ6AiOoHEhPAMlG35wcDFg2pgalJQTeU7QQedIW5H3CPgOoemZoHhMYMt6nDyGrLPAirJy-2VjdQ/p.jpeg?fv_content=true&size_mode=5"
+          "https://ucc5d3bc832b958dec203f3ab4c5.previews.dropboxusercontent.com/p/thumb/ABFfaTI-rb8c3k5zxgIMGOg03Pv5m3Plav6hEF17d36XDWOJWOevRK5q-XGnTTeqbO6SxkrPJMKnOJC97Y7rXBIsqk77KBowFU48RJvnlzM6wdL8-Y3IvF4YhjHhJfuuWRvGl3HMkb1bVd_aB5Ucw8oQX2GcF-TV71ZJ6oPYaww_oyEXuXHAtG3ikpVZyqdVKk8l6JRdcnoxcy2yqNBIeuoiOfmmVOywzaWwfJGVkdlZ2p_j55xzROk1Q-2MuoifdPMqXeaflCT67FkZpXuEV3e2AFug_kSyuB23l3u4MX4JeSOlFiERaoWc6HMYfobLu5bEIEBMFPr-wijXsSsAiSNAil8CcjV3h_7j6sn8gfy_eg/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         time={"36:22"}
@@ -79,12 +79,12 @@ function Cards() {
 
       <Card
         src={
-          "https://ucff9a44fbcc9381e87b04bd331c.previews.dropboxusercontent.com/p/thumb/ABEtWsLr6ocbzexG4cBLnnN_yJ9-Ygkq9lDnslE0hm9hejHc7du46BE7Ms2sNYsk5-8COtWJeQ7j3guJSf2jznj8R2qMXDXLb57LmmNuIKz3WJcuF8kF4bGEtalvQOoxVr5lC554vKFbf0WCYhweo5ihWfg33nPmzHuGpVSf0Yh-P2J8-R1IFJDz0PvZ7cbI-iRITVuhZS6vSSF5YXH8fMi3VRrL12GqqI-D-l6HZZNZWxasdwIdnMgw0zDytIiBQlrTkoXlUB0CRSGPjrP_whdHKDpc2JWRs4vnIkvVwrmlaSDgUeqBgsMpknAIqiAxsVHsNV7-Kdx0AciViy4tON3pP6LdT3zat1Fj9ui5qV9R7g/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc8bfa738566cac00c4da717f62d.previews.dropboxusercontent.com/p/thumb/ABGK3r-awCruobP2YHw3A64OFh-37LK4JlUGK6z79lDknE-YfAcH4837MXTaIlNtS1l6RC9DnxmjYqb_WYwnTk8LuS2cJ_Z8vDHyPGI-5FVFQitC1AIuv_DHI0rdlsUJxJnEQTFCChUdnwMvs49WbQ6Qnon9C4DisuHNmru_22yFWIWlSp9p0GG9uFVTSImuPicBrV1nr-DqZArbPnTRoFNWITSa4AFCM6urh3sFUeo-DIYKCqgMKuQIbkVEs9cazJD4RVuFDdCUh31cFzC1vPk4bHzDajGzyyunoe8PEbyoDwyalc9kHxXHZAb29ReR6g8LpVrEtIHrn5qVtuokyAA7XXa6OQvoYFWunxdzmxHssw/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"HIIT Series"}
         description={"Full-Body HIIT Series"}
         profileSrc={
-          "https://uc15d678cc458ecad5c415b52b7e.previews.dropboxusercontent.com/p/thumb/ABGnfC4PM6FofqisWaXJ8in6iUcNdJ-S473K5mZ7XaOlXW7UgbWnQiKxhpgHZJ_w1ThfRjPseSZ_4VwDujHTFq_yh7fTJ8xGQfPLIhw37Lji1F8DjMCgZDTvfKFFYkmgnh8Ojd-N2s70sLCwFEasxpK_ofkzxAeMM-Exsxh6c827y0ztTw0toUNR7VrjyFBwcGQ_y5HeOUQo4sx2D30L1OlgbshDjhtywqk1xfDtQ_r9oq90jAJBunethBvP2T8YV3uddM8AKKyJBkWmFbw_8_wZALwx5zQeWl-n437c6X1Wy3kdW7frbe-L0T0mV-7ulmkwAQu8BQSC6_MV1jI4aGDAgjsGl4X_MfIs8g8DYmqxrQ/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc55f0b3aefbf4857e4a31f0fe3d.previews.dropboxusercontent.com/p/thumb/ABFn8seYK2cWAY4PqGDUosq05Iq6p5o7qidUKTDIuWDlN9VkxB0KomaWPQJRX2SSvbUanaEeQCV_0q680vbONAafBXNLTmQC1VYiAGGU3zT6l8wxjUGUxKMAs55Dwn4vxH1y7M_3KhkUzRL0XFWmWWYEFC3qkxI4GzZP2yOudHyeSvs2gTroWpGQmxfTPejkFPl7AvedOQS0nFq3v51mPZG9t0nQPEtcmNUKy6WBcHKximgmM_gPaMAu61ajn9Ymkg7RTPEUhjlpmLmJhBLTgFsSbhg_D5OXe1qxDPPnQ0SqJrwnZjqP2bDzulX0dmBN-o7UsEKIz1AcXqaP60wziiU1oWIQ_tyRgdyYak5-wJ0P5g/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         icons={false}
@@ -92,12 +92,12 @@ function Cards() {
 
       <Card
         src={
-          "https://uc8194e10c1a52cd8b2e4578aa0c.previews.dropboxusercontent.com/p/thumb/ABHRE6TnIw9Wfc723hPV38X0c08RLST9u6799SBv84qRiO1Mf0EIVSK-fcYydQC0x4kJf5SHtvwbNuM4ENUJ3PbRI8OWBVv0GNHzNseFpvh-VYtGqAKL2dVxezsapMYmANEASqjyR6xKICWaJM1mptui8dLOL8jgshzpY8Zrz462K1pPk97v0u7xKeZsU63qc52ylMYN1hr8sd_6xcWtGUrDoNT5Zb9aNCzRvTdYe1GmrYESGq9ZXbH302HuJK4MYwktXetuKY6bGHJw-BpYQOTLdfiMUIvPHEjQWdiDalzV_tbEcs_CTYDfB8JXh8dkx-CyMC-Hy-vYjP_WnqQD8Mk_imOf8ycoTe6H3B6Rvn6P8g/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc1fb13d648d0b3868787a5470f1.previews.dropboxusercontent.com/p/thumb/ABFLtKfMaGHJnFoYE8gy7kqMGuRkGeqx7sr5F-kcWK1akOT3A_FJpkIj4PbiM9o6zgt0VPqTvJ8_YSLeVVW5OkSOPtCec-UhzsRq6CNEuYD7VXezdyKFnbLTlJr3M22-rvhjIRk-vDVtzGn1vcLF8BVQhh9S3fx7L0eeTbVPGR4C4b4zB2oMruFdYC-8t94QUijmpRNz6aXJpV_Xx2ab5VIrywsyvleD8TeAvLI1L5olf8aHbjHCZbAdLeaJscCtdiBaLUA4QHMVnKs5Kb0dMAbOmd2z76CasD5jE75wgH6QFMKNN4w6tyWQdUVyLo2_Qczs0c5HQ3XbHsPCTpzkO_CpQTELDjGQQBVXkY94gudlEA/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"Kafue River"}
         description={"Kafue River, Zambia - Power Stroke Pyramid"}
         profileSrc={
-          "https://ucc63ec9d97f98dafcb3c3c62303.previews.dropboxusercontent.com/p/thumb/ABHhwCIbwQVTTz-HC9lqlI37BfnTkEAR4I-m8zNIUmtqJFzwV5X1zR5i54L5sCEwnL9BE7s7IdlmZipUSYLQY75i0zlTauPdK4AWsrDM0464S2WDFw5-8efojUj5Om8f1Xq3v1ZnJDjVu0l6_s7TOTrRnYhQbwwGnII5XXtNQThel1lMi_dV4P0IXVKmguZtP07T_YbrQov_YZAWS_PqkqqtkdJL79PJkyfhdGS_klj8crUPhulQomDTg9rJRn289-CbjF-sY4fUa9TUa04dbCWzQS8m_CbYFZQTBjns14bgxYb1q9UutgkSJ--Y5uHRBwEJh22B3IJij9HU8MsJTzcKuWDAIEyWHxOeAJ7inogAsw/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc0193d69478ef81295f636f1a56.previews.dropboxusercontent.com/p/thumb/ABEtd100VYFPe1VJJCiI8jqiwqjOuG9Lidu84YMkm8kjbYDqPrwVmo51ylJTEXXXMz0ZFmZVpafJrUl4308VAiWSPaNQfQhmHkx22ClVR9qbsNd0JgPHqmvJfZ_o6qr_bA1dafbPsfd1Dxmp-Ehzp5-x5_ThwUSGQ9IUFKWlfC_C6Nd2Ji0zvrP_SvB9H2vqdLkS8pws0bspKsVbBbRQHq_kUgjk8WWRx4GxM4_tRB6fyll6BNHSylUB2ma-oks3GMIMx76DO8ODE2SSVZE8eR-XYy-ZEpoLO-O8QMwXRGJrGXBM19EgQKNy_dp-05aHkhpBPSEBC6QJo-8H9VX3cnj_bd1yOFKesor8EW4983r9kA/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         time={"22:22"}
@@ -107,12 +107,12 @@ function Cards() {
 
       <Card
         src={
-          "https://ucaa578c634771d40661eaba0209.previews.dropboxusercontent.com/p/thumb/ABEpKUIT-COYpHvB0qRI1dftDJclZRtAyD-0l9iedmQNfEzo7nf2aDDl0QmIf4HtmLNfWsHKszbKbUQ6dlljq3r6zfDHTYPzZLh1KuxF5nkBthPBky-6xZWtk3gN1zYuXab7KWhgWnaCeU7uLQ-LdFMrSYcxdI5Ucbh_kppTAfZ_hECb1GC0jn6ENV3kTehOJO897PtOSqZfQB4AyECWZaFU8SZOkd0rUtOZAZpaFJQLzUvGL3oD_gwaGzPrhyECfcrRqVkkAMS6Lk76awiSLrT5OA2gMitmt880ADI1tb9HaQlLjsANAsVt-HmO-CqP7reeOnxWueliEVQ31pXrBiH68jMXn32hVb6v74PxJCZXXw/p.jpeg?fv_content=true&size_mode=5"
+          "https://uc6e1afe32f78ea07a7f6a2d1788.previews.dropboxusercontent.com/p/thumb/ABF51MK5XtpfTgYwbC7NOYYaJ5pWdgENyvMro19qGDrlVBEwx5tbDCsdxhlDrY3seEog-v3J_no5gRoXeQW6KYTK6hUMMXZDj4qbzZEpw-4_eOYPuB6LSJ3-Ryjxn3RB0Ji9i535F0xWfjR1eac1dTi5AdohK5Bqw3AkiTXdf-FuY5pGxjUOLyb4BdVPGOMeERsvI0XCLAAYImdTlWoFnb1OJGGwuRsu1e3CdfnLbtp2E5W5-as1f3Qc_O7VaME26l_sujIqMLPboLRxdZ9SlOFvnlzfOMNaBrxDEAbx2xMBqJlRqqJ4m_S9grFRwKp311OZQwR7m6ZSuzSS3KQ_VdGgaD26ZX9sSZDRSumw-AEvyw/p.jpeg?fv_content=true&size_mode=5"
         }
         alt={"Shred & Burn"}
         description={"Shred & Burn Series"}
         profileSrc={
-          "https://uc4e5de5dfef85652572e9dbeb23.previews.dropboxusercontent.com/p/thumb/ABGHzMbcbiI0o9j7JwpC5qWE4dWHVAv3MubF4fpp48T2Rp419aPLNSnfG06_0NaSOOrjWsNuLKRvAXK97n0LCL4uiW6LEQfno66sr-zTISoH67PCmW-inkdAd43pCkAwMkN-F1ojbpzbUwLi8l5lKR2SyreTG41MPxWoLiG3fOdvb0mPwsJqG-A3JwDaSoTJJtsCNTLHCsm-hlmGH2GaiH4Ux7NjCP_jUAQs-QEkSvWXOub3pZLa_7C4Uy0S8D9PY7j11pJYn81HP0G1YS1zXFvID15a-NMAY63fQMs5JN5uRMNyiJ5c6YpqL-3L585LXxRcIW0QaEjf_hrj1k6JPLANxfqXXPfhkc9ivr4s0B3tHA/p.jpeg?fv_content=true&size_mode=5"
+          "https://ucfad352528b9a4824c2657a23de.previews.dropboxusercontent.com/p/thumb/ABE3639kqAk4Qkh2kbnQcxONXn5a9fErn_VIbzClFIjelpyIeN3Dq4YqkmtM2KZJbmBWC_0MYO57624q_ORo4Bbs_BtDIEG4vsTgCxrhAf1bedaUT0dIVHqUfdHJzCcO9GnAmLBL5kUD82XC2ShCQJ7vomUKdRiASn9CnYKuM7DlqOmAF8PO4F3SLuJNfEGrH97jKdGwlkvW4f0Gxhx5NAPkQzqaWyTH9x3xzlOPkDZjhqoYZrR3weesGtZLNzUTcPhofNBD2vC3jE9IxIKrtW_IuK0BhHtOMpyDdAyWtdC0dU98QDHxrqPo8euYKhUJFVL7CA3XMnmyf63k4DDj_TZxgcVtS5gWN_wfdiuPTzPhow/p.jpeg?fv_content=true&size_mode=5"
         }
         profileAlt={"instructor"}
         icons={false}
